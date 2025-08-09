8e8b6e386e067ae1266b126c858c3f80c8df19b9
Mohamed djawad Abi Ayad <abi_ayad.mohamed_djawad@courrier.uqam.ca>
2025-08-09 12:22:57 -0400

    Config access roles

---
+++ profilService/src/main/java/com/onlineLibrary/profil/Flux/JwtAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.onlineLibrary.profil.Flux;
+
+import com.onlineLibrary.profil.Flux.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String email;
+        final String role;
+
+        // Verify if  header Authorization is included and begin with "Bearer "
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7); // remove "Bearer "
+
+        try {
+            // extract email + role from jwt token
+            email = jwtService.extractEmail(jwt);
+            role = jwtService.extractRole(jwt);
+
+            if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                // Create authority from role (ex : ROLE_ADMIN)
+                SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role);
+
+                // Create  Authentication token
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(email, null, List.of(authority));
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+
+        } catch (Exception e) {
+            SecurityContextHolder.clearContext();
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid JWT token");
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+}
--- profilService/src/main/java/com/onlineLibrary/profil/config/SecurityConfig.java
+++ profilService/src/main/java/com/onlineLibrary/profil/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.onlineLibrary.profil.config;
 
+import com.onlineLibrary.profil.Flux.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,21 +8,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtFilter) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // souvent désactivé pour API REST
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/profil/**").permitAll()
+                        .requestMatchers("/api/verif/profil/actuator/adminEndpoint").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/api/verif/profil/actuator/userEndpoint").hasAuthority("ROLE_USER")
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 

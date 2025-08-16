b6bd7bcdffd97a328bf691b28168692f288b0ea4
Mohamed djawad Abi Ayad <abi_ayad.mohamed_djawad@courrier.uqam.ca>
2025-08-15 22:43:26 -0400

    Config angular webapp intgration in docker network

--- webapp_interactive/src/app/pages/login/login.component.ts
+++ webapp_interactive/src/app/pages/login/login.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 
 // Angular Material imports
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
+import {AuthService} from '../../services/auth';
+import {RegisterModel} from '../../models/register/register.model';
+import {Credentials} from '../../models/login/credentials.model';
 
 @Component({
   selector: 'login-page',
@@ -25,9 +28,22 @@ export class LoginComponent {
   email = '';
   password = '';
 
+  constructor(private router: Router, private authService:AuthService) {}
+
+
   onLogin() {
-    console.log('Email:', this.email, 'Password:', this.password);
-    // Ajouter logique d'authentification
+    const credentials = new Credentials(this.email, this.password);
+    this.authService.login(credentials).subscribe({
+      next: (response) => {
+        console.log('Login réussi', response);
+        // redirection après succès
+        // this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        console.error('Erreur login', err);
+        // afficher un message d'erreur à l'utilisateur si nécessaire
+      }
+    });
   }
 
   loginWithFacebook() {
